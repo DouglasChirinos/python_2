a822a72f7a8c046edbdf3aaaaf270e958c93442d
DouglasChirinos <50874012+DouglasChirinos@users.noreply.github.com>
2025-02-27 23:23:53 -0400

    [ADD] Hack 4

--- hack_4.py
+++ hack_4.py
@@ -9,5 +9,9 @@ text: "qux" output => "qux"
 
 def fn_hack_4(s):
     result = s
-    #...
+    if len(result)>3:
+        result = result[1:len(result)-1]
     return result
+print(fn_hack_4('fooziman'))
+print(fn_hack_4('barziman'))
+print(fn_hack_4('qux'))

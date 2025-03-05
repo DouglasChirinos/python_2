9f0689302468328633aeb8530158fb246dd39f51
DouglasChirinos <50874012+DouglasChirinos@users.noreply.github.com>
2025-03-04 21:29:45 -0400

    ADD Hack python 2

--- hack_6.py
+++ hack_6.py
@@ -11,5 +11,14 @@ text: [] output => ["0"]
 
 def fn_hack_6(s):
     result = s
-    #...
+    if len(result) == 0:
+        result =["0"]
+    else:
+        for k in range(len(result)):
+            if k % 2 != 0: xchar = "-"
+            else: xchar = str(k+1)     
+            result[k] = xchar
     return result
+
+print(fn_hack_6(["a","b","c","d","e"]))
+print(fn_hack_6([]))
--- hack_7.py
+++ hack_7.py
@@ -11,5 +11,16 @@ text: [0] output => [0]
 
 def fn_hack_7(s):
     result = s
-    #...
+    for k in range(len(result)):
+        if  type(result[k]) == str: 
+            if ((k+1) % 2 == 0):
+                result[k]  =  (k+1)
+            else:
+                result[k]  = str(k+1)
+        elif type(result[k]) == int:
+             result[k]  =  k
+       
     return result
+
+print(fn_hack_7(["a","b","c","d","e"]))
+print(fn_hack_7([0]))
